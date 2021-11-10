b796431fd1fc46cf7fd7590b833249419a8421ab
Marius Tolzmann <marius@mineiros.io>
2021-11-10 23:24:19 +0100

    chore: reformat versions.tf

--- versions.tf
+++ versions.tf
@@ -1,7 +1,14 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# SET TERRAFORM AND PROVIDER REQUIREMENTS FOR RUNNING THIS MODULE
+# ---------------------------------------------------------------------------------------------------------------------
+
 terraform {
-  required_version = ">= 0.14.7, < 2.0"
+  required_version = ">= 0.14, < 2.0"
 
   required_providers {
-    google = "~> 3.75"
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 3.75"
+    }
   }
 }
